c75e84def452dcca352c8c2c3f169c3229b737f5
FranAlc <fgd.galan@gmail.com>
2023-08-09 15:49:17 -0300

    Teoria lenguaje C

---
+++ Lenguaje C/Ejercicios/01_teoria.c
@@ -0,0 +1,30 @@
+/*
+#Tipos de datos
+
+Entero -> Almacenamiento de valores enteros(sin decimal). // INT
+Real -> Permite almacenar valores decimales.	// FLOAT o DOUBLE -> double es para datos de mayor tamaño
+Carácter -> Almacenan un carácter alfanumérico. // CHAR
+Lógico o Bool -> True o False.	// BOOL
+Texto o Cadena -> Almacenamiento de Texto.
+
+#Instrucciones primitivas
+
+Asignaciones 
+Operaciones(+, - , * , /, ..)
+Identificadores(nombres de variables o constantes)
+Valores(Números o texto encerado entre comillas)
+Llamadas a subprogramas
+
+#Operadores Lógicos
+> Mayor que
+< Menor que
+>= Mayor o igual
+<= Menor o igual
+!= Distinto
+== Igual 
+
+AND && 
+OR || 
+
+
+*/
