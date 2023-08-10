fa12834f8dab7367ddafe06a84e49812cc4a6abb
FranAlc <fgd.galan@gmail.com>
2023-08-10 03:23:31 -0300

    Teoria general

---
+++ Lenguaje C/Ejercicios/00_teoria.c
@@ -0,0 +1,23 @@
+/* Diseño de estrategia */
+
+/*
+1. Análisis del problema, identificar correctamente los Datos, el Proceso y el Resultado
+Datos -> Cuáles son los datos del problema.
+Proceso -> Qué operaciones debo hacer, qué procedimiento debo llevar a cabo para devolver un resultado.
+Resultado -> Cuál es el resultado que me pide.
+
+2. Diseño de la estrategia, defino como voy a diseñar el algoritmo y doy un orden lógico a las tareas que voy a realizar.
+
+Entradas -> Ingreso el valor de las variables.
+Proceso -> Elaboro el desarrollo del programa.
+Salida -> Devuelvo lo que pide la consigna.
+
+#Nuestra tarea deberá comenzar analizando profundamente los requerimientos del
+problema, familiarizándonos con el enunciado leyéndolo cuidadosamente, resaltando
+las partes más importantes y dificultosas de interpretar.
+Si el problema lo permite conviene apoyarse en soluciones gráficas, confección de
+tablas o cualquier otro elemento que nos facilite la comprensión.
+
+*/
+
+
